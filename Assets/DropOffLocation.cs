53ac27fdd38c9620941b6ee0d5a17abb9bd56312
Finn Pauls <mbp@pauls.dev>
2022-10-06 16:54:22 +0200

    Rotate room on finish

--- Assets/DropOffLocation.cs
+++ Assets/DropOffLocation.cs
@@ -7,9 +7,36 @@ public class DropOffLocation : MonoBehaviour
     int numberOfPickUps;
     int countDropOffs;
 
+    bool isDone = false;
+    bool rotateBack = false;
+
+    public GameObject room;
+
     void Start() {
         var pickUps = GameObject.FindGameObjectsWithTag("Pickup");
         numberOfPickUps = pickUps.Length;
+    }
+
+    void Update() { 
+        var rotation = room.transform.rotation.eulerAngles;
+        if (isDone) {
+            if (rotation.y < 180) {
+                room.transform.Rotate(0, 1f, 0);
+            }
+
+            if (Input.GetMouseButtonDown(0)) {
+                rotateBack = true;
+            }
+        }
+
+        if (rotateBack) {
+            if (rotation.y > 0)  {
+               room.transform.Rotate(0, 1f, 0);
+            } else {
+                rotateBack = false;
+                isDone = false;
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other) {
@@ -18,6 +45,7 @@ public class DropOffLocation : MonoBehaviour
 
             if (countDropOffs == numberOfPickUps) {
                 Debug.Log("You win!");
+                isDone = true;
             } else {
                 Debug.Log("You have " + countDropOffs + " out of " + numberOfPickUps + " items");
             }
@@ -29,4 +57,5 @@ public class DropOffLocation : MonoBehaviour
             countDropOffs--;
         }
     }
+
 }
